f19a31ac71b189c51b4ff5a56b2440a24f845199
Darin Krauss <darin@tidepool.org>
2021-02-11 18:01:14 -0800

    LOOP-2534 Refactor onboarding and support manager plugins (#1)
    - https://tidepool.atlassian.net/browse/LOOP-2534
    - https://tidepool.atlassian.net/browse/LOOP-2955
    - Create initial TidepoolOnboarding project and targets
    - Move onboarding from TidepoolService plugin/repo
    - Use PrescriptionReviewUICoordinator as temporary onboarding

---
+++ TidepoolOnboardingPlugin/TidepoolOnboardingPlugin.h
@@ -0,0 +1,17 @@
+//
+//  TidepoolOnboardingPlugin.h
+//  TidepoolOnboardingPlugin
+//
+//  Created by Darin Krauss on 12/10/20.
+//  Copyright © 2021 Tidepool Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TidepoolOnboardingPlugin.
+FOUNDATION_EXPORT double TidepoolOnboardingPluginVersionNumber;
+
+//! Project version string for TidepoolOnboardingPlugin.
+FOUNDATION_EXPORT const unsigned char TidepoolOnboardingPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TidepoolOnboardingPlugin/PublicHeader.h>

437fd70a164ba2238871215bbf97a841780c2378
Rick Pasetto <rickpasetto@yahoo.com>
2021-09-07 13:18:14 -0700

    LOOP-3736, LOOP-3737, LOOP-3738: Instructional copy content updates
    https://tidepool.atlassian.net/browse/LOOP-3736
    https://tidepool.atlassian.net/browse/LOOP-3737
    https://tidepool.atlassian.net/browse/LOOP-3738

--- TidepoolOnboarding/Views/Sections/IntroductionViews.swift
+++ TidepoolOnboarding/Views/Sections/IntroductionViews.swift
@@ -121,7 +121,7 @@ fileprivate struct IntroductionView4: View {
         Segment(header: LocalizedString("Tidepool Loop’s Connection", comment: "Onboarding, Introduction section, view 4, segment 2, header")) {
             PresentableImage(decorative: "Introduction_4_2")
             Paragraph(LocalizedString("The app connects directly to your devices via a Bluetooth connection and automates insulin dosing with or without an internet connection.", comment: "Onboarding, Introduction section, view 4, segment 2, paragraph 1"))
-            Paragraph(LocalizedString("Your iPhone or iPod Touch must be on and within Bluetooth range of your pump in order for the app to send commands to the pump.", comment: "Onboarding, Introduction section, view 4, segment 2, paragraph 2"))
+            Paragraph(LocalizedString("Caution: Your iPhone or iPod Touch must be on and within Bluetooth range of your pump in order for the app to send commands to the pump.", comment: "Onboarding, Introduction section, view 4, segment 2, paragraph 2"))
         }
     }
     
@@ -129,7 +129,7 @@ fileprivate struct IntroductionView4: View {
         Segment(header: LocalizedString("Interacting with the App", comment: "Onboarding, Introduction section, view 4, segment 3, header")) {
             Paragraph(LocalizedString("You will interact with the Tidepool Loop app to:", comment: "Onboarding, Introduction section, view 4, segment 3, paragraph"))
             BulletedBodyTextList(
-                LocalizedString("Enter your personal diabetes settings", comment: "Onboarding, Introduction section, view 4, segment 3, list, item 1"),
+                LocalizedString("Enter your personal diabetes settings (Caution: Users should work with their healthcare practitioners (HCPs) when first setting up their personal settings)", comment: "Onboarding, Introduction section, view 4, segment 3, list, item 1"),
                 LocalizedString("Deliver bolus insulin for food and to bring down high glucose", comment: "Onboarding, Introduction section, view 4, segment 3, list, item 2"),
                 LocalizedString("Make temporary glucose range adjustments", comment: "Onboarding, Introduction section, view 4, segment 3, list, item 3"),
                 LocalizedString("View glucose history and insulin delivery history", comment: "Onboarding, Introduction section, view 4, segment 3, list, item 4"),
@@ -199,6 +199,7 @@ fileprivate struct IntroductionView7: View {
                 LocalizedString("Automation", comment: "Onboarding, Introduction section, view 7, list, item 9")
             )
             Paragraph(LocalizedString("If there is a concept you’d like to review, consult the Tidepool Loop User Guide for more details.", comment: "Onboarding, Introduction section, view 7, paragraph 2"))
+            Paragraph(LocalizedString("A complete list of insulin pumps and CGMs that can be used with the Tidepool Loop app is provided in the Tidepool Loop User Guide.", comment: "Onboarding, Introduction section, view 7, paragraph 3"))
         }
     }
 }

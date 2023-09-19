cae1a18879d8d459add67e173d1c615aa27a43ca
Nathaniel Hamming <nate@hmtconsulting.org>
2023-09-19 05:47:19 -0300

    [COASTAL-1291] adding tidepool security (#99)
    * adding tidepool security
    
    * response to PR comment

--- TidepoolOnboardingTests/Mocks/MockOnboardingProvider.swift
+++ TidepoolOnboardingTests/Mocks/MockOnboardingProvider.swift
@@ -10,6 +10,7 @@ import LoopKit
 import LoopKitUI
 
 class MockOnboardingProvider: OnboardingProvider {
+    
     var allowDebugFeatures: Bool = true
 
     func getNotificationAuthorization(_ completion: @escaping (NotificationAuthorization) -> Void) { completion(.notDetermined) }
@@ -45,6 +46,10 @@ class MockOnboardingProvider: OnboardingProvider {
         .failure(MockError())
     }
     
+    func statefulPlugin(withIdentifier identifier: String) -> StatefulPluggable? {
+        nil
+    }
+
     var availableSupports: [SupportUI] = []
     
     var onboardingTherapySettings = TherapySettings()
